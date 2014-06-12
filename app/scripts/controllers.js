0a503ef3de10f1ab8d75545de643d07f83f84bcb
Loïc Knuchel <loicknuchel@gmail.com>
2014-06-12 11:28:17 +0200

    add feature list to home screen

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,5 +7,36 @@ angular.module('IonicBoilerplate.controllers', [])
 
 .controller('HomeCtrl', function($scope) {
   'use strict';
+  $scope.state = {
+    showDelete: false,
+    showReorder: false
+  };
+  
+  $scope.deleteFeatures = function(){
+    $scope.state.showDelete = !$scope.state.showDelete;
+    $scope.state.showReorder = false;
+  };
+  $scope.reorderFeatures = function(){
+    $scope.state.showDelete = false;
+    $scope.state.showReorder = !$scope.state.showReorder;
+  };
 
+  $scope.edit = function(feature) {
+    alert('Edit Feature: ' + feature.name);
+  };
+  $scope.share = function(feature) {
+    alert('Share Feature: ' + feature.name);
+  };
+
+  $scope.moveFeature = function(feature, fromIndex, toIndex) {
+    $scope.features.splice(fromIndex, 1);
+    $scope.features.splice(toIndex, 0, feature);
+  };
+  $scope.onFeatureDelete = function(feature) {
+    $scope.features.splice($scope.features.indexOf(feature), 1);
+  };
+  
+  $scope.features = [
+    {link: 'aaa', name: 'Slide box'}
+  ];
 });
